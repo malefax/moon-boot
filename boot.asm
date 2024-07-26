89eb2c9256ea68ee65d7c64cbad581a6eb9c7764
malefax <shiven522002@gmail.com>
2024-07-26 05:53:38 -0500

    x86 real mode boot

---
+++ boot.asm
@@ -0,0 +1,39 @@
+ORG 0 ;assume logical adress be 0
+BITS 16    ; 1Mib of memory just to ensure each instruction should be 16 bits
+_origion:
+	jmp short start
+	nop
+	times 33 db 0
+start:
+	jmp 0x7c0:step1
+
+step1:
+	cli ; clear interupt
+	mov ax,0x7c0
+	mov ds,ax
+	mov es,ax
+	mov ax,0x00
+	mov ss,ax
+	mov sp,0x7c00
+	sti ; Enable interupt
+	mov si, message
+	call print
+	jmp $
+print:
+	mov bx,0
+.loop:
+	lodsb
+	cmp al, 0
+	je .done
+	call print_char
+	jmp .loop
+.done:
+	ret
+print_char:
+	mov ah ,0eh
+	int 0x10
+	ret
+
+message: db "welcome to moon os...-_-",0
+times 510-($-$$) db 0
+dw 0xAA55
